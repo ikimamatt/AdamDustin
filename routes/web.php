90752df86aa5f08f04a4cf4fc47ac87a8e50975b
ikimamatt <ikimamat0@gmail.com>
2025-01-30 19:08:28 +0800

    login & fixed bug ui admin

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,25 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
+
+
+
+
+// Rute autentikasi
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+// Rute admin (protected)
+Route::middleware('admin')->prefix('admin')->group(function () {
+    Route::get('', function () {
+        return view('admin.index');
+    })->name('dashboard');
+    Route::get('/news', function () {
+        return view('admin.news');
+    })->name('admin.new');
+});
 
 Route::get('/', function () {
     return view('Dashboard');
@@ -12,15 +31,4 @@ Route::get('/Berita', function () {
     return view('Berita');
 });
 
-Route::get('/admin', function () {
-    return view('admin.index');
-})->name('dashboard');
-
-
-Route::get('/login', function () {
-    return view('auth.login');
-});
-
-Route::get('/news.admin', function () {
-    return view('admin.news');
-})->name('admin.new');
+/
